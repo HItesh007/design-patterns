a597ab0d011ec5555a2bc47f75265981f3fa32ae
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2021-05-17 12:10:04 +0530

    ShapeFactory: Extension of AbstractFactory is added to demonstrate AbstractFactory Pattern

--- src/main/java/com/projects/designpatterns/creational/factory/ShapeFactory.java
+++ src/main/java/com/projects/designpatterns/creational/factory/ShapeFactory.java
@@ -1,5 +1,6 @@
 package com.projects.designpatterns.creational.factory;
 
+import com.projects.designpatterns.creational.abstractFactory.AbstractFactory;
 import com.projects.designpatterns.creational.enums.ShapeType;
 
 /**
@@ -7,9 +8,13 @@ import com.projects.designpatterns.creational.enums.ShapeType;
  * @version 1.0
  * @implNote This a an actual factory class implementation. Using this class, Objects of various implementation
  * of Factory/Overridden method is called.
+ *
+ * <pre>Note : This class extends {@link AbstractFactory}. And this extension is to demonstrate Abstract Factory pattern.
+ * </pre>
  */
-public class ShapeFactory {
+public class ShapeFactory extends AbstractFactory {
 
+    @Override
     public Shape getShape(ShapeType shapeType) {
         return switch (shapeType) {
             case CIRCLE -> new Circle();
